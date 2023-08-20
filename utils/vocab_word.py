52b3c9c8f29ae7d4a5f5680ef232c0074dc51491
doubleK29 <khanh.techbk@gmail.com>
2023-08-20 17:21:40 +0000

    Refactor code

--- utils/vocab_word.py
+++ utils/vocab_word.py
@@ -2,31 +2,43 @@ import torch
 
 
 class Vocab:
+    """Vocabulary class"""
+
     def __init__(self, vocab_file):
-        self.vocab_to_id = dict()
-        self.id_to_vocab = dict()
+        """Initialize special tokens
+
+        Args:
+            vocab_file (txt file): txt file of Vietnamese tokenized vocab
+        """
+        self.vocab_to_id = {}
+        self.id_to_vocab = {}
         with open(vocab_file, "r", encoding="utf8") as rf:
             lines = rf.read().splitlines()
-            # print(f"Number of vocab lines: {len(lines)}")
             for idx, line in enumerate(lines):
                 self.vocab_to_id[line] = idx
                 self.id_to_vocab[idx] = line
         self.sos_id = self["<sos>"]
         self.eos_id = self["<eos>"]
         self.pad_id = self["<pad>"]
+        self.unk_id = self["<unk>"]
 
     def __getitem__(self, token):
+        """Get encoded token
+        Returns: index of input token
+        """
         return self.vocab_to_id[token]
 
     def __len__(self):
+        """Get length of vocabulary"""
         return len(self.vocab_to_id)
 
     def get_all_vocab(self):
+        """Get all vocabulary tokens"""
         return list(self.vocab_to_id.keys())
 
     def encode(self, words):
         """
-        string convert to ids
+        Convert token to index
         """
         ids = []
         for i in words:
@@ -41,7 +53,7 @@ class Vocab:
 
     def decode(self, ids):
         """
-        ids: list of id
+        Convert index to token
         """
         words = []
         for i in ids:
@@ -50,8 +62,9 @@ class Vocab:
 
 
 if __name__ == "__main__":
-    vocab = Vocab("utils/vocab/vocab.tgt")
-    encode = vocab.encode("xin (c#hao) em iu".split())
-    print(encode[0])
+    vocab = Vocab(
+        "/home/khanh/workspace/Cinnamon2023-HaNoiTeam/utils/vocab/tokenize_tone.txt"
+    )
+    encode = vocab.encode("toi di hoc".split())
     decode = vocab.decode(encode.tolist())
     print(encode, "\n", decode, "\n")
